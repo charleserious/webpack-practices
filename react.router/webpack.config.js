cc321c936dc126f2cf1780186ccf865abda64479
Charleserious <>
2018-02-01 10:24:44 +0800

    add React Route practice

---
+++ react.router/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ 'es2015', 'react' ]
+                    }
+                }
+            }
+        ]
+    }
+}
